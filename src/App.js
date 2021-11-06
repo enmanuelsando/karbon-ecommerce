039669395e6eb1c3ba14c51c1a0adf789db0c15b
enmanuelsando <enmanuelsando@gmail.com>
2021-11-06 16:36:52 -0300

    Se agrega menu en app.js y se instalan librerias MUI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
     <div className="App">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+             <MenuIcon />
+           </IconButton>
+            <Typography variant="h6"    component="div" sx={{ flexGrow: 1 }}>
+             Karbon
+          </Typography>
+          <Button color="inherit">Register</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
       <header className="App-header">
+        <Button variant='contained' color='secondary' size='medium'>
+          Hello World
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
