81d1acba08799e378dc92607e2ea92f1ca41bc1f
enmanuelsando <enmanuelsando@gmail.com>
2021-11-07 23:27:06 -0300

    Se edita barra de menu de header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import Button from '@mui/material/Button'
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import FadeMenu from './components/menu'
 
 function App() {
   return (
     <div className="App">
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <AppBar position="static" color="default">
           <Toolbar>
             <IconButton
               size="large"
@@ -21,13 +21,13 @@ function App() {
               aria-label="menu"
               sx={{ mr: 2 }}
             >
-             <MenuIcon />
+             <FadeMenu/>
            </IconButton>
             <Typography variant="h6"    component="div" sx={{ flexGrow: 1 }}>
              Karbon
           </Typography>
-          <Button color="inherit">Register</Button>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" variant="outlined">Register</Button>
+          <Button color="inherit" variant="outlined">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
---
+++ src/components/menu.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Fade from '@mui/material/Fade';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function FadeMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <MenuIcon
+        id="fade-button"
+        aria-controls="fade-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Dashboard
+      </MenuIcon>
+      <Menu
+        id="fade-menu"
+        MenuListProps={{
+          'aria-labelledby': 'fade-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}>Home</MenuItem>
+        <MenuItem onClick={handleClose}>Products</MenuItem>
+        <MenuItem onClick={handleClose}>About</MenuItem>
+      </Menu>
+    </div>
+  );
+}
